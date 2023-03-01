b3f89ae1e7b27458dfaec4e83429bee392e59797
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-03-01 23:54:28 +0300

    add user interface

---
+++ src/interface/user.interface.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+export interface UserDocument extends mongoose.Document {
+  email: string
+  name: string
+  password: string
+  createdAt: Date
+  updatedAt: Date
+  comparePassword(canditatePassword: string): Promise<boolean>
+}
