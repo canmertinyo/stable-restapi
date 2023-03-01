da3adf0a2a51fd8081e6e72045edbc4acdb28d66
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-03-01 23:38:30 +0300

    add database connection

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import config from 'config'
 
+import { connectDatabase } from './db/connect'
+
 const port = config.get('port') as number
 const host = config.get('host') as string
 
@@ -11,4 +13,5 @@ app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, host, () => {
   console.log(`server listening at http://${host}:${port}`)
+  connectDatabase()
 })
---
+++ src/db/connect.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import config from 'config'
+
+export async function connectDatabase(): Promise<void> {
+  const dbUri = config.get('dbUri') as string
+
+  mongoose.set('strictQuery', true)
+
+  return mongoose
+    .connect(dbUri)
+    .then(() => console.log('database connected'))
+    .catch((err: any) => {
+      console.log('connection failed!' + err)
+    })
+}
