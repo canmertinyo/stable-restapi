9e9c2ef04c9057ec97eeacc8db12070c9a3c60f5
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-03-02 13:38:13 +0300

    fix imports on route

--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,9 @@ import { Express, Request, Response } from 'express'
 import config from 'config'
 
 import { createUserHandler } from './controller/user.controller'
+import { createUserSessionHandler } from './controller/session.controller'
 
 const prefix = config.get('prefix') as string
-
-console.log(prefix)
 
 export default function (app: Express): void {
   app.get('/healthcheck', (req: Request, res: Response) => res.sendStatus(200))
